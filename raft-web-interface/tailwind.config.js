6fbfc1535ed82a6814b2fe66e2dbd5e7acd5df14
Michael Lim <michaellimbong@gmail.com>
2022-03-10 23:06:34 +0000

    feat: use open sans font

--- raft-web-interface/tailwind.config.js
+++ raft-web-interface/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Open Sans', 'Helvetica', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
