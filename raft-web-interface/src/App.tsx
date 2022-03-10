5c75af7fa1a8aa1e49ca14b7d7008b2603752194
Michael Lim <michaellimbong@gmail.com>
2022-03-10 17:14:31 +0000

    feat: add max width to interface

--- raft-web-interface/src/App.tsx
+++ raft-web-interface/src/App.tsx
@@ -6,7 +6,7 @@ import ServerBlock from './components/ServerBlock';
 
 const App: FC = () => {
   return (
-    <div className="container mx-auto py-4 px-2">
+    <div className="container mx-auto py-4 px-6 max-w-6xl">
       <h1 className='text-center text-3xl font-bold'>Raft Consensus Algorithm Interface</h1>
       <ConnectionStatus />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
