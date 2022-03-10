4d46a275857509840c9c735ef6951d6f5a636c25
Michael Lim <michaellimbong@gmail.com>
2022-03-10 17:09:45 +0000

    feat: add current time and leader visualization

--- raft-web-interface/src/components/ConnectionStatus.tsx
+++ raft-web-interface/src/components/ConnectionStatus.tsx
@@ -1,7 +1,8 @@
-import { IConnectionType } from "customTypes/server";
+import { IConnectionType, IServerState } from "customTypes/server";
 import { raftClient } from "libs/RaftClient";
 import { useObservableState } from "observable-hooks";
 import { FC } from "react";
+import { filter, map } from "rxjs";
 import ConnectionButton from "./ConnectionButton";
 
 const getStatusMessage = (status?: IConnectionType) => {
@@ -18,13 +19,25 @@ const getStatusClass = (status?: IConnectionType) => {
   return "text-red-500";
 }
 
+const CurrentLeader: FC = () => {
+  const [leaderServerId] = useObservableState(() => raftClient.latestDetailsUpdateMessages.pipe(
+    filter((it) => it.data.state === IServerState.LEADER),
+    map((it) => it.data.id)
+  ))
+
+  return (
+    <p className="text-center font-bold text-xl">Current Leader 👑: {leaderServerId !== undefined ? `Server ${leaderServerId + 1}` : 'None'}</p>
+  );
+}
+
 const ConnectionStatus: FC = () => {
   const [status] = useObservableState(() => raftClient.latestConnectionStatus);
 
   return (
     <div className="mt-2">
-      <div>
+      <div className="mb-4">
         <p className="text-center font-bold text-xl">Connection Status: <span className={getStatusClass(status)}>{getStatusMessage(status)}</span></p>
+        <CurrentLeader />
       </div>
       <div className="text-center mt-2">
         <button className="mr-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-300" onClick={() => raftClient.restartRaft()}>
--- raft-web-interface/src/components/ServerBlockHeader.tsx
+++ raft-web-interface/src/components/ServerBlockHeader.tsx
@@ -1,4 +1,8 @@
+import { IServerState } from "customTypes/server";
+import { raftClient } from "libs/RaftClient";
+import { useObservableState } from "observable-hooks";
 import { FC } from "react";
+import { distinct, filter, map, tap } from "rxjs";
 import DuplicatedMessagesButton from "./DuplicatedMessagesButton";
 import ServerConnectionButton from "./ServerStatusButton";
 
@@ -8,6 +12,37 @@ interface IServerBlockHeaderProps {
   toggleShowDuplicated: () => void;
 }
 
+const ServerCurrentTime: FC<{ serverId: number }> = ({ serverId }) => {
+  const [currentTime] = useObservableState(() => raftClient.latestDetailsUpdateMessages.pipe(
+    filter((it) => it.data.id === serverId),
+    map((it) => it.time),
+    distinct()
+  ))
+
+  return (
+    <p className="text-xl text-center mb-2">(Current Time: {currentTime?.toFixed(2)} ms)</p>
+  )
+}
+
+const ServerIsLeader: FC<{ serverId: number }> = ({ serverId }) => {
+  const [isLeader] = useObservableState(() => raftClient.latestDetailsUpdateMessages.pipe(
+    filter((it) => it.data.id === serverId),
+    map((it) => it.data.state),
+    tap((it) => {
+      console.log(serverId, it)
+    }),
+    map((it) => it === IServerState.LEADER)
+  ));
+
+  if (!isLeader) {
+    return null;
+  }
+
+  return (
+    <span>(Leader 👑)</span>
+  )
+}
+
 const ServerBlockHeader: FC<IServerBlockHeaderProps> = ({
   showDuplicated,
   serverId,
@@ -15,7 +50,8 @@ const ServerBlockHeader: FC<IServerBlockHeaderProps> = ({
 }) => {
   return (
     <>
-      <h2 className='text-center font-bold text-2xl'>Server {serverId + 1}</h2>
+      <h2 className='text-center font-bold text-2xl'>Server {serverId + 1} <ServerIsLeader serverId={serverId} /></h2>
+      <ServerCurrentTime serverId={serverId} />
       <div className="mb-2 flex justify-between">
         <DuplicatedMessagesButton serverId={serverId} showDuplicated={showDuplicated} onClick={toggleShowDuplicated} />
         <ServerConnectionButton serverId={serverId} />
