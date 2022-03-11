1c0db72206fc5e722b603a2b5e146543cfc5e2ed
Michael Lim <michaellimbong@gmail.com>
2022-03-11 04:57:58 +0000

    feat: add server controls beside visualisation panel

---
+++ raft-web-interface/src/components/ServerStatusVisualisationButton.tsx
@@ -0,0 +1,47 @@
+import { FC } from "react";
+import { Button } from "react-bootstrap";
+import { IServerStatusValue } from "../customTypes/server";
+import { useObservableState } from "observable-hooks";
+import { raftClient } from "libs/RaftClient";
+import { useCallback } from "react";
+
+const getMessage = (status?: IServerStatusValue) => {
+  switch (status) {
+    case IServerStatusValue.RESTARTED: return "Stop";
+    default: return "Start";
+  }
+}
+
+const getButtonClass = (status?: IServerStatusValue) => {
+  switch (status) {
+    case IServerStatusValue.RESTARTED: return "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-300";
+    default: return "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-300";
+  }
+}
+
+export interface IServerStatusVisualisationButtonProps {
+  serverId: number;
+  className?: string;
+}
+
+const ServerStatusVisualisationButton: FC<IServerStatusVisualisationButtonProps> = ({
+  serverId,
+  className,
+}) => {
+  const [serverStatus] = useObservableState(() => raftClient.getLatestServerStatusById(serverId));
+  const onClick = useCallback(() => {
+    if (serverStatus === IServerStatusValue.HALTED) {
+      raftClient.startServer(serverId);
+    } else {
+      raftClient.stopServer(serverId);
+    }
+  }, [serverStatus, serverId]);
+
+  return (
+    <Button onClick={onClick} className={[getButtonClass(serverStatus), className ?? ''].join(' ')}>
+      {getMessage(serverStatus)} Server {serverId}
+    </Button>
+  );
+}
+
+export default ServerStatusVisualisationButton;
